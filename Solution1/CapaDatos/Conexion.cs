5018d6e019390c45869cf7604f4fcd4c072835b7
ahenriquez94 <47991678+ahenriquez94@users.noreply.github.com>
2019-05-21 20:06:25 -0600

    Add files via upload

---
+++ Solution1/CapaDatos/Conexion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        String cadenaconex = "Data Source=.;Initial Catalog=hospital;Integrated Security=True";
+        SqlConnection cn=new SqlConnection();
+        SqlCommand cmd;
+        SqlDataReader dr;
+        
+
+        //public Conexion()
+        //{
+            //cn = new SqlConnection("Data Source=.;Initial Catalog=hospital;Integrated Security=True");
+            //cn.Open();
+        //}
+
+        public SqlConnection Conex
+        {
+            get { return cn; }
+            set { cn = value; }
+        }
+
+        public bool Conectar()
+        {
+            bool conn = false;
+            try
+            {
+                cn.ConnectionString = cadenaconex;
+                cn.Open();
+                conn = true;
+            }
+            catch (Exception ex)
+            {
+                conn = false;
+            }
+            return conn;
+        }
+        public void Desconectar()
+        {
+            cn.Close();
+        }
+
+        public void consultaSQL(String consulta)
+        {
+            String mensaje = "Error";
+            try
+            {
+                cmd = new SqlCommand(consulta, cn);
+                cmd.ExecuteNonQuery();
+                mensaje = "Correctamente conectado";
+            }
+            catch
+            {
+                mensaje ="Error";
+            }
+        }
+    }
+}
---
+++ Solution1/CapaMantenimiento/Mantenimiento.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+
+namespace CapaMantenimiento
+{
+    public class Mantenimiento
+    {
+        Conexion cn = new Conexion();
+
+        public String Especialidad
+        {
+            get { return especialidad; }
+            set { especialidad = value; }
+        }
+
+        public String Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        private int doctor_no, hospital_cod;
+
+        public int Hospital_cod
+        {
+            get { return hospital_cod; }
+            set { hospital_cod = value; }
+        }
+
+        public int Doctor_no
+        {
+            get { return doctor_no; }
+            set { doctor_no = value; }
+        }
+        private String apellido, especialidad;
+
+       
+
+        
+
+        public void insertardoctor()
+        {
+            try
+            {
+                String insert = "INSERT INTO Doctor(Doctor_No,Hospital_Cod,Apellido,Especialidad)VALUES('" + doctor_no + "','" + hospital_cod + "','" + apellido + "','" + especialidad + "')";
+                cn.Conectar();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
